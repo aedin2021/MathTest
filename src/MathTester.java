dd4af404b9bf69b4dd0645a74632808991ceb522
aedin <aedin.rundell.2021@student.lhschools.org>
2018-09-10 13:55:54 -0400

    updated

--- src/MathTester.java
+++ src/MathTester.java
@@ -14,18 +14,20 @@ public class MathTester {
         firstNumber = 5;
         secondNumber = 300;
         Scanner key = new Scanner(System.in);
-
+        int imput;
         firstWord = "thing";
         secondWord = " ohter thing";
 
 
-        System.out.print(firstWord+secondWord);
-       /* System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+      /*  System.out.print(firstWord+secondWord);
+      *//* System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
         System.out.println("firstNumber / secondNumber is "+ (firstNumber/secondNumber));
         System.out.println("The modulus of the firstNumber by the secondNumber is "+ (firstNumber%secondNumber));*/
-
+        System.out.println("hello");
+        imput = key.nextByte();
+        System.out.println((imput*5));
 
 
 
